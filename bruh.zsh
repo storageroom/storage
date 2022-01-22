50418f701e6f173fabe703e11dff343b1987fc80
Joshua Tsao <Joshua@Joseos.com>
2022-01-22 13:29:23 +0800

    yesyyyhhjisisis
    no

---
+++ bruh.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/zsh
+curl https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh --output /usr/share/zsh-sudo/sudo.plugin.zsh
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git /usr/share/zsh-sudo
+curl https://github.com/Joseos123/shell/blob/main/linux/zshrc --output ~/.zshrc
