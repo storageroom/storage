817b60e47c689fd8710747c6b34e34e120d58db7
Joshua Tsao <Joshua@Joseos.com>
2022-01-26 09:17:12 +0800

    Update bruh.zsh

--- bruh.zsh
+++ bruh.zsh
@@ -1,5 +1,6 @@
 #!/usr/bin/zsh
+sudo mkdir /usr/share/zsh-sudo
 curl https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh --output /usr/share/zsh-sudo/sudo.plugin.zsh
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git /usr/share/zsh-sudo
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git /usr/share/
 curl https://github.com/Joseos123/shell/blob/main/linux/zshrc --output ~/.zshrc
 curl https://raw.githubusercontent.com/Joseos123/shell/main/macos/starship.toml --output ~/.config/starship.toml
