a2612bf80df30b7e5d6787facd918da0008149d0
Joshua Tsao <Joshua@Joseos.com>
2022-02-01 15:07:08 +0800

    😶‍🌫️

--- bruh.zsh
+++ bruh.zsh
@@ -7,8 +7,8 @@
 
 if which curl >/dev/null; then
 else
-echo "curl is not installed on this system and is needed for later steps."
-echo "Do you wish to install curl now?"
+echo "\n\ncurl is not installed on this system and is needed for later steps."
+echo "Do you wish to install curl now?\n\n"
 select yn in "Yes" "No"; do
   case $yn in
    Yes ) sudo apt install curl -y; break;;
@@ -20,7 +20,7 @@ fi
 if which git >/dev/null; then
 else
 echo "git is not installed on this system and we reccomend you install it."
-echo "Do you wish to install git now?"
+echo "Do you wish to install git now?\n\n"
 select yn in "Yes" "No"; do
   case $yn in
    Yes ) sudo apt install git -y; break;;
@@ -31,7 +31,7 @@ fi
 
 if [ -d "/usr/share/zsh-sudo" ] 
 then
-    echo "Directory zsh-sudo already exists, will not install zsh-sudo." 
+    echo "Directory zsh-sudo already exists, will not install zsh-sudo.\n\n" 
     echo "run:"
     echo "curl https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh --output /usr/share/zsh-sudo/sudo.plugin.zsh"
     echo "to manually install it instead"
@@ -42,7 +42,7 @@ fi
 
 if [ -d "/usr/share/fast-syntax-highlighting" ] 
 then
-    echo "Directory fast-syntax-highlighting already exists, will not install fast-syntax-highlighting." 
+    echo "Directory fast-syntax-highlighting already exists, will not install fast-syntax-highlighting.\n\n" 
     echo "run:"
     echo "git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git /usr/share/fast-syntax-highlighting"
     echo "to manually install it instead"
@@ -52,41 +52,74 @@ else
     if which git >/dev/null; then
     git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git /usr/share/fast-syntax-highlighting
     else
-    echo "git is not installed on this system and required to install this file."
-    echo "Do you wish to install git now?"
+    echo "git is not installed on this system and required to install this file.\n\n"
+    echo "Do you wish to install git now?\n\n"
       select yn in "Yes" "No"; do
         case $yn in
          Yes ) sudo apt install git -y;git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git /usr/share/fast-syntax-highlighting; break;;
-          No ) echo "run:";echo "git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git /usr/share/fast-syntax-highlighting";echo "to manually install it instead";break;;
+          No ) echo "run:";echo "git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git /usr/share/fast-syntax-highlighting";echo "to manually install it instead\n\n";break;;
        esac
       done
     fi
 fi
 
-echo "install zshrc. continue?"
+echo "install zshrc. continue?\n\n"
 select yn in "Yes" "No"; do
   case $yn in
     Yes ) curl https://raw.githubusercontent.com/Joseos123/shell/main/linux/zshrc --output ~/.zshrc; break;;
-    No ) break;;
+    No ) echo "run:\n";echo "curl https://raw.githubusercontent.com/Joseos123/shell/main/linux/zshrc --output ~/.zshrc\n";echo "to manually install it instead\n\n";break;;
   esac
 done
 
 echo "if u need it:"
-echo "curl https://raw.githubusercontent.com/Joseos123/shell/main/linux/config.yml --output config.yml"
+echo "\ncurl https://raw.githubusercontent.com/Joseos123/shell/main/linux/config.yml --output config.yml\n"
 echo "your welcome, myself"
+echo "\n\n"
 
-echo "install starship config. continue?"
-select yn in "Yes" "No"; do
-  case $yn in
-    Yes ) break;;
-    No ) exit;;
-  esac
-done
+echo "install starship?/n/n"
+  select yn in "Yes" "No"; do
+    case $yn in
+      Yes ) istar=true;;
+      No ) istar=false;;
+    esac
+  done
 
-if [ -d "~/.config" ] 
-then
-    curl https://raw.githubusercontent.com/Joseos123/shell/main/macos/starship.toml --output ~/.config/starship.toml
+if [ "$istar" = true ] ; then
+  stari=true
+  sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+
+elif [ "$istar" = false ] ; then
+  stari=false
+  echo "run:\n"
+  echo 'sh -c "$(curl -fsSL https://starship.rs/install.sh)"\n'
+  echo "to manually install it instead\n\n"
+
+else 
+  echo "how did u break my script :/"
+  echo "\n\n"
+fi
+
+if [ "$stari" = true ] ; then
+  echo "install starship config. continue?\n\n"
+    select yn in "Yes" "No"; do
+      case $yn in
+        Yes ) starc=true;;
+        No ) starc=false;;
+      esac
+    done
 else
-    mkdir ~/.config
-    curl https://raw.githubusercontent.com/Joseos123/shell/main/macos/starship.toml --output ~/.config/starship.toml
+  starc=false
+fi
+
+if [ "$starc" = true ] ; then
+    if [ -d "~/.config" ] 
+    then
+        curl https://raw.githubusercontent.com/Joseos123/shell/main/macos/starship.toml --output ~/.config/starship.toml
+    else
+        mkdir ~/.config
+        curl https://raw.githubusercontent.com/Joseos123/shell/main/macos/starship.toml --output ~/.config/starship.toml
+    fi
+
+elif [ "$starc" = false ] ; then
+      echo "have a nice day!"
 fi
