79fe4d8f0bd75a067e930c2d70ef14e24bf3209e
Joshua Tsao <Joshua@Joseos.com>
2022-02-16 07:35:21 +0800

    🥹

--- server.bash
+++ server.bash
@@ -9,7 +9,7 @@ CYAN="\e[1;36m"
 NC='\033[0m'
 
 # check dist
-printf "\n${GREEN}what distribution are on on?${NC}\n"
+printf "\n${GREEN}what linux distribution are on on?${NC}\n"
 select os in "Debian" "Arch"; do
 	case $os in
 	Debian)
