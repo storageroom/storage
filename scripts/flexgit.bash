89e87632ae1d1788bebd02d5a66cc44922a3618b
joshy <joshua@joseos.com>
2022-02-24 12:26:39 +0800

    🌈

--- scripts/flexgit.bash
+++ scripts/flexgit.bash
@@ -2,4 +2,5 @@
 git add .
 usuasuausuausausuasu=$(date +%d/%m)
 git commit -m "🌈 $usuasuausuausausuasu"
-git push origin main
+git push origin main || exit
+curl https://betteruptime.com/api/v1/heartbeat/zfnS1uFSeYdSwQY41Na7mMRW
